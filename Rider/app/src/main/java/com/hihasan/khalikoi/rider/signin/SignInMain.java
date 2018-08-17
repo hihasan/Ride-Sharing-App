d50d6c2a1ea618cf8838a721cd43007fd4faae0a
hihasan <hasan@hihasan.com>
2018-08-17 13:02:33 +0600

    Basic

--- Rider/app/src/main/java/com/hihasan/khalikoi/rider/signin/SignInMain.java
+++ Rider/app/src/main/java/com/hihasan/khalikoi/rider/signin/SignInMain.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.hihasan.khalikoi.rider.R;
+
 public class SignInMain extends AppCompatActivity
 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
     }
 }
