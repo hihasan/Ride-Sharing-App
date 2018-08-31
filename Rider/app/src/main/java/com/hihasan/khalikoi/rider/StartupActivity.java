5f208c66eb033751704db0676492483b9e9bffb7
hihasan <hasan@hihasan.com>
2018-08-31 12:33:51 +0600

    Satrtup Animation

--- Rider/app/src/main/java/com/hihasan/khalikoi/rider/StartupActivity.java
+++ Rider/app/src/main/java/com/hihasan/khalikoi/rider/StartupActivity.java
@@ -8,6 +8,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.hihasan.khalikoi.rider.signin.SignInMain;
@@ -15,11 +18,15 @@ import com.hihasan.khalikoi.rider.signin.SignInMain;
 public class StartupActivity extends AppCompatActivity
 {
     public final int APP_LOADING_TIME=3000;
+    private ImageView logo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
 
+        logo=(ImageView) findViewById (R.id.logo);
+        logoAnimation();
         //Thread Handler for 5 sec.
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -28,6 +35,13 @@ public class StartupActivity extends AppCompatActivity
             }
         }, APP_LOADING_TIME);
 
+    }
+
+    //Animation
+    private void logoAnimation()
+    {
+        Animation animator= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.blink);
+        logo.startAnimation(animator);
     }
 
     //Check connection Here
