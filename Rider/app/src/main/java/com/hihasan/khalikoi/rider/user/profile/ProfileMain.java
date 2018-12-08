d2228586ffbd51bf0d8e7d6c85dfec9fb5d31097
hihasan <hasan@hihasan.com>
2018-12-08 13:50:05 +0600

    profile need to work on it

--- Rider/app/src/main/java/com/hihasan/khalikoi/rider/user/profile/ProfileMain.java
+++ Rider/app/src/main/java/com/hihasan/khalikoi/rider/user/profile/ProfileMain.java
@@ -4,78 +4,171 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import com.hihasan.khalikoi.rider.R;
+import com.hihasan.khalikoi.rider.trips.TripList;
+import com.hihasan.khalikoi.rider.util.HttpServicesClass;
+import com.hihasan.khalikoi.rider.util.ListAdapterClass;
 import com.hihasan.khalikoi.rider.util.Value;
 import com.hihasan.khalikoi.rider.util.HttpParse;
 
 public class ProfileMain extends AppCompatActivity
 {
-    HttpParse httpParse = new HttpParse();
-    ProgressDialog pDialog;
-
-    // Http Url For Filter Student Data from Id Sent from previous activity.
-    String HttpURL = "http://bachaw.com/api/rider_profile.php";
-
-    // Http URL for delete Already Open Student Record.
-    //String HttpUrlDeleteRecord = "http://bachaw.com/api/rider_profile.php";
-
-    String finalResult ;
-    HashMap<String,String> hashMap = new HashMap<>();
-    String ParseResult ;
-    HashMap<String,String> ResultHash = new HashMap<>();
-    String FinalJSonObject ;
-    TextView NAME,PHONE_NUMBER, PASSWORD;
-    String NameHolder, NumberHolder, ClassHolder;
-    Button UpdateButton, DeleteButton;
-    String TempItem;
-    ProgressDialog progressDialog2;
+    ListView StudentListView;
+    ProgressBar progressBar;
+    String HttpUrl = "http://bachaw.com/api/trip_list.php";
+    List<String> IdList = new ArrayList<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_trip);
 
-        NAME = (TextView)findViewById(R.id.name);
-        PHONE_NUMBER = (TextView)findViewById(R.id.phone);
-        PASSWORD= (TextView) findViewById (R.id.password);
-        if (Value.rider==null){
-            NAME.setText("Rider Name: Bachaw");
-            PHONE_NUMBER.setText("Rider Phone: "+Value.rider_phone);
-            PASSWORD.setText("Rider Password: "+Value.password);
+        StudentListView = (ListView)findViewById(R.id.listview1);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        new TripList.GetHttpResponse(ProfileMain.this).execute();
+
+        //Adding ListView Item click Listener.
+        StudentListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // TODO Auto-generated method stub
+
+                Toast.makeText(getApplicationContext(),"User Profile",Toast.LENGTH_SHORT).sh
+
+                //Finishing current activity after open next activity.
+                finish();
+
+            }
+        });
+    }
+
+    // JSON parse class started from here.
+    private class GetHttpResponse extends AsyncTask<Void, Void, Void>
+    {
+        public Context context;
+
+        String JSonResult;
+
+        List<Value> studentList;
+
+        public GetHttpResponse(Context context)
+        {
+            this.context = context;
         }
 
-        else {
-            NAME.setText("Rider Name: "+Value.rider);
-            PHONE_NUMBER.setText("Rider Phone: "+Value.rider_phone);
-            PASSWORD.setText("Rider Password: "+Value.password);
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
         }
 
+        @Override
+        protected Void doInBackground(Void... arg0)
+        {
+            // Passing HTTP URL to HttpServicesClass Class.
+            HttpServicesClass httpServicesClass = new HttpServicesClass(HttpUrl);
+            try
+            {
+                httpServicesClass.ExecutePostRequest();
 
-        //CLASS = (TextView)findViewById(R.id.textClass);
+                if(httpServicesClass.getResponseCode() == 200)
+                {
+                    JSonResult = httpServicesClass.getResponse();
 
-        //UpdateButton = (Button)findViewById(R.id.buttonUpdate);
-       // DeleteButton = (Button)findViewById(R.id.buttonDelete);
+                    if(JSonResult != null)
+                    {
+                        JSONArray jsonArray = null;
 
-        //Receiving the ListView Clicked item value send by previous activity.
-//        TempItem = getIntent().getStringExtra("ListViewValue");
-//        Intent intent=getIntent();
-//        String str=intent.getStringExtra("name");
-//        PHONE_NUMBER.setText(str);
-//        //Calling method to filter Student Record and open selected record.
-//        HttpWebCall(Value.name);
+                        try {
+                            jsonArray = new JSONArray(JSonResult);
+
+                            JSONObject jsonObject;
+
+                            Value ride;
+
+                            studentList = new ArrayList<Value>();
+
+                            for(int i=0; i<jsonArray.length(); i++)
+                            {
+                                ride = new Value();
+
+                                jsonObject = jsonArray.getJSONObject(i);
+
+                                // Adding Student Id TO IdList Array.
+                                IdList.add(jsonObject.getString("rides_id").toString());
+                                System.out.println("Rider Phone: "+Value.rider_phone);
+                                if (jsonObject.getString("rider_phone").toString().equals(Value.rider_phone)){
+                                    //Adding Student Name.
+                                    ride.RideListRider =("User Phone: "+jsonObject.getString("rider_phone").toString());
+                                    ride.RideListDriver =("Driver Name:"+jsonObject.getString("driver_name").toString()) ;
+                                    ride.RideListFare = ("Fare: "+jsonObject.getString("fare").toString());
+                                }
+
+                                else {
+                                    Toast.makeText(getApplicationContext(),"No Rides FOund",Toast.LENGTH_SHORT).show();
+                                }
 
 
+
+                                studentList.add(ride);
+
+                            }
+                        }
+                        catch (JSONException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                else
+                {
+                    Toast.makeText(context, httpServicesClass.getErrorMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result)
+
+        {
+            progressBar.setVisibility(View.GONE);
+
+            StudentListView.setVisibility(View.VISIBLE);
+
+            ListAdapterClass adapter = new ListAdapterClass(studentList, context);
+
+            StudentListView.setAdapter(adapter);
+
+        }
     }
 
 
